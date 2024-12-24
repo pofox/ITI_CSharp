85bb1e364d4daed1e39df2bf3bd9d65ebd42aaf4
pofo <pofo>
2024-12-24 15:03:25 +0200

    tasks 3 & 4

--- ITI_C#/Point3D.cs
+++ ITI_C#/Point3D.cs
@@ -6,38 +6,57 @@ using System.Threading.Tasks;
 
 namespace ITI_C_
 {
-    internal class Point3D
+    public class Point3D
     {
-        public float x {  get; set; }
-        public float y { get; set; }
-        public float z { get; set; }
-        Point3D()
+        public int x {  get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+        public Point3D()
         {
             x = default;
             y = default;
             z = default;
         }
-        Point3D(float _x, float _y, float _z):this(_x, _y)
+        public Point3D(int _x, int _y, int _z):this(_x, _y)
         {
             z = _z;
         }
-        Point3D(Point3D p)
+        public Point3D(Point3D p)
         {
             x = p.x;
             y = p.y;
             z = p.z;
         }
-        Point3D(float _x)
+        public Point3D(int _x)
         {
             x = _x;
         }
-        Point3D(float _x,float _y):this(_x)
+        public Point3D(int _x,int _y):this(_x)
         {
             y = _y;
         }
         public static implicit operator string(Point3D p)
         {
             return $"Point Coordinates: ({p.x}, {p.y}, {p.z})";
+        }
+        public static bool operator ==(Point3D p1, Point3D p2)
+        {
+            return p1.Equals(p2);
+        }
+        public static bool operator !=(Point3D p1, Point3D p2)
+        {
+            return !p1.Equals(p2);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is Point3D))
+                return false;
+            Point3D p = (Point3D)obj;
+            if (GetType() != p.GetType())
+                return false;
+            return x == p.x && y == p.y && z == p.z;
         }
         public override string ToString()
         {
--- ITI_C#/Program.cs
+++ ITI_C#/Program.cs
@@ -4,16 +4,53 @@
     {
         static void Main(string[] args)
         {
-            float x, y, z;
-            Point3D p1, p2;
+            int x, y, z;
+            Point3D p1 = new Point3D(), p2 = new Point3D();
             Console.Write("P1.x = ");
-            while (!float.TryParse(Console.ReadLine(),out x))
+            while (!int.TryParse(Console.ReadLine(),out x))
             {
-                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Error Try Again");
-                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write("P1.x = ");
             }
+            Console.Write("P1.y = ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("P1.z = ");
+            z = int.Parse(Console.ReadLine());
+            p1.x = x;
+            p1.y = y;
+            p1.z = z;
+            Console.Write("P2.x = ");
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error Try Again");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("P2.x = ");
+            }
+            Console.Write("P2.y = ");
+            while (!int.TryParse(Console.ReadLine(), out y))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error Try Again");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("P2.y = ");
+            }
+            Console.Write("P2.z = ");
+            while (!int.TryParse(Console.ReadLine(), out z))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error Try Again");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("P2.z = ");
+            }
+            p2.x = x;
+            p2.y = y;
+            p2.z = z;
+            Console.WriteLine(p1);
+            Console.WriteLine((string)p2);
+            Console.WriteLine(p1 == p2 ? "P1 == P2" : "P1 != P2");
         }
     }
 }
