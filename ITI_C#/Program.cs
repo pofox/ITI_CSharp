b89bd53286c9923e51674cc2ca7e678fa6860c3c
pofo <pofo>
2024-12-25 09:25:11 +0200

    .

--- ITI_C#/Program.cs
+++ ITI_C#/Program.cs
@@ -10,7 +10,21 @@
             list.Add(2, "two");
             list.Add(5, "five");
             list.Add(3, "three");
-            Console.WriteLine(list[1].ToString());
+            PrintKeysAndValues(list);
+            list.SetValueAtIndex(4, "V");
+            PrintKeysAndValues(list);
+            Console.WriteLine($"list contains key 3 : {list.ContainsKey(3)}");
+            Console.WriteLine($"list capacity : {list.Capacity}");
+            Console.WriteLine($"list count : {list.Count}");
+            list.Remove(4);
+            PrintKeysAndValues(list);
+            Console.WriteLine($"list contains value \"five\" : {list.ContainsValue("five")}");
+            list.RemoveAt(2);
+            PrintKeysAndValues(list);
+            Console.WriteLine($"index of key 1 : {list.IndexOfKey(1)}");
+            Console.WriteLine($"index of value three : {list.IndexOfValue("three")}");
+            list.Clear();
+            PrintKeysAndValues(list);
         }
         public static void PrintKeysAndValues(SortedList<int,string> myList)
         {
